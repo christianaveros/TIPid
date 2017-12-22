582046838568eb21010c836b5fb4276df1020f2a
christian averos <christianaveros@github.com>
2017-12-22 22:28:50 +0800

    Modified index html, better handing of GET form in views.py

--- TIPid/mainsite/views.py
+++ TIPid/mainsite/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from .models import Search_bar
 
-# Create your views here.
 def index(request):
-	if(request.method == 'POST'):
-		content = {
-			'int': request.POST['int']
-		}
-	else:
-		content = {
-			'int': '0'
+	content = {
+		'search': request.GET['search'] if 'search' in request.GET else ''
 		}
 	return render(request, 'index.html', content)
