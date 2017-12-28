9d1d3b2af49395ca5db64c08aec6db39a9da96c2
jobea <jobea.delicano@gmail.com>
2017-12-28 13:34:22 +0800

    added models in mainsite and shared task for shopee scraper

--- TIPid/mainsite/models.py
+++ TIPid/mainsite/models.py
@@ -1,15 +1,80 @@
 from django.db import models
 from django.utils import timezone
 
-class History(models.Model):
-	history = models.CharField(max_length=200)
-	create_at = models.DateTimeField(default=timezone.now(), blank=True)
-	def __str__(self):
-		return self.history
-
-
 class Item(models.Model):
 	title = models.CharField(max_length=200) # title name of item
-	information = models.TextField() # links. info. specs.
+	create_at = models.DateTimeField(default=timezone.now(), blank=True)
 	def __str__(self):
 		return self.title
+
+class ScrapedProducts(models.Model):
+	
+	WEBSITES =(
+		('amazon', 'Amazon.com'),
+		('lazada', 'Lazada.com.ph'),
+		('shopee', 'Shopee.ph')
+	)
+	
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    name = models.CharField(max_length=512, blank=False)
+	website = models.CharField(max_length=32, choices=WEBSITES)
+	url = models.URLField()
+    price = models.IntegerField(blank=False)
+	original_price = models.IntegerField()
+	rating = models.DecimalField(blank=False)
+	reviews = models.IntegerField(blank=False)
+	bayes_est = models.DecimalField(blank=False)
+    def __str__(self):
+        return self.title
+
+
+"""
+
+class ScrapedAmazonProducts(models.Model):
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    name = models.CharField(max_length=512, blank=False)
+	url = models.URLField()
+    price = models.IntegerField(blank=False)
+	original_price = models.IntegerField()
+	rating = models.DecimalField(blank=False)
+	reviews = models.IntegerField(blank=False)
+	bayes_est = models.DecimalField(blank=False)
+    def __str__(self):
+        return self.title
+
+class ScrapedLazadaProducts(models.Model):
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    name = models.CharField(max_length=512, blank=False)
+    price = models.IntegerField(blank=False)
+	original_price = models.IntegerField()
+	rating = models.DecimalField(blank=False)
+	reviews = models.IntegerField(blank=False)
+	bayes_est = models.DecimalField(blank=False)
+    def __str__(self):
+        return self.title
+
+class ScrapedShopeeProducts(models.Model):
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    name = models.CharField(max_length=512, blank=False)
+    price = models.IntegerField(blank=False)
+	original_price = models.IntegerField()
+	rating = models.DecimalField(blank=False)
+	reviews = models.IntegerField(blank=False)
+	bayes_est = models.DecimalField(blank=False)
+
+    def __str__(self):
+        return self.title
+
+class RankedProductList(models.Model):
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	name = models.CharField(max_length=512, blank=False)
+    price = models.IntegerField(blank=False)
+	original_price = models.IntegerField()
+	rating = models.DecimalField(blank=False)
+	reviews = models.IntegerField(blank=False)
+	bayes_est = models.DecimalField(blank=False)
+
+    def __str__(self):
+        return self.title
+
+"""
--- TIPid/mainsite/tasks.py
+++ TIPid/mainsite/tasks.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import json
 import requests
 import re
+from selenium import webdriver
 
 @shared_task
 def add(x, y):
@@ -27,3 +28,20 @@ def scrapers(search_term):
 	soup = BeautifulSoup(data, 'html.parser')
 	jsondata = json.loads(soup.find('script', text=re.compile	(r'window.pageData=')).get_text().replace('window.pageData=', ''))
 	return jsondata
+
+@shared_task
+def scraper_shopee(search_term):
+	browser = webdriver.Chrome()
+	browser.get('https://shopee.ph/search/?is_official_shop=1&keyword=' + search_term + '&page=0')
+	soup = BeautifulSoup(browser.page_source)
+	items = soup.find_all(class_="shopee-search-result-view__item-card")
+	item_list = []
+	"""
+	for item in items:
+		item.append{
+			'name': item.find(class_=""),
+			'price': item.find(class_="", text=re.compile(r'₱(...)+₱|,|₱')),
+			'rating': item.find_all(class_=""),
+			'reviews': item.find(class_="")
+		}
+	"""
