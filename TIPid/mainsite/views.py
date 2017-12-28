4e4086684319a9f5f1918d9ee0a2ed5a5339b05b
christian averos <christianaveros@github.com>
2017-12-28 21:47:13 +0800

    Set up ajax. removed footer/header html. add base html

--- TIPid/mainsite/views.py
+++ TIPid/mainsite/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
-def index(request):
-	content = {
-		'search': request.GET['search'] if 'search' in request.GET else ''
-		}
-	return render(request, 'index.html', content)
+class IndexView(CreateView):
+	template_name = 'index.html'
+	form_class = UserCreationForm
+
+def search(request):
+	search = request.GET.get('search', None)
+	#itemlist = Call a model here that returns a JSON data
+	if (itemlist['data'] == None): 
+		itemlist['empty'] = True;
+	return JsonResponse(itemlist)
+	#content = {
+	#	'search': request.GET['search'] if 'search' in request.GET else ''
+	#	}
+	#return render(request, 'index.html', content)
